2a86ae30ec287653ba57adebe53276334ba83a91
georgeli88 <gli88@hotmail.com>
2014-10-23 20:00:19 +0800

    update files

--- README.md
+++ README.md
@@ -14,12 +14,19 @@ You should create one R script called run_analysis.R that does the following.
 5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 ## How run_analysis.R script works 
-1. Assume the following 3 data files are in the current working directory
-	* "X_test.txt"
-	* "X_train.txt"
+1. Assume the following 2 data files are in the current working directory
 	* "features.txt"
-2. Open R Studio
-3. Use setwd("Current_Working_Directory") to set to the working directory
+	* "activity_labels.txt"
+2. Assume the following 3 data files are in the sub-directory "test" under the current working directory
+	* "X_test.txt"
+	* "y_test.txt"
+	* "subject_test.txt"
+3. Assume the following 3 data files are in the sub-directory "train" under the current working directory
+	* "X_train.txt"
+	* "y_train.txt"
+	* "subject_train.txt"
+4. Open R Studio
+5. Use setwd("Current_Working_Directory") to set to the working directory
 4. Run the following command
 	* source("run_analysis.R")
 	* run_analysis()
@@ -27,26 +34,46 @@ You should create one R script called run_analysis.R that does the following.
 ## What is the design of the script
 1. Read the test data "X_test.txt" into a test data set called "testdata"
 	* There are 2947 obs. of 561 variables.
-2. Read the train data "X_train.txt" into a train data set called "traindata"
+2. Read the test activity and test subject files 
+	* Each one has 2947 obs. of 1 variable
+3. Read the train data "X_train.txt" into a train data set called "traindata"
 	* There are 7352 obs. of 561 variables.
-3. Use rbind() to merge the train data set and the test data set to create one data set called "data"
+4. Read the train activity and train subject files 
+	* Each one has 2947 obs. of 1 variable
+5. Use rbind() to merge the train data set and the test data set to create one data set called "data"
 	* The merged data set has 10299 obs of 561 variables
-4. Read "features.txt" to extract only the measurements of mean for each measurement
+6. Use rbind() to merge the train activity and the test activity to create one data set called "activitydata"
+	* The merged activitydata has 10299 obs of 1 variable
+	* Rename the variable name to be "Activity"
+7. Use rbind() to merge the train subject and the test subject to create one data set called "subjectdata"
+	* The merged subjectdata has 10299 obs of 1 variable
+	* Rename the variable name to be "Subject"
+8. Read the "activity_labels.txt" file to get the activity names
+	* Change the activity number to activirt name in the "activitydata" data set
+9. Read "features.txt" to extract only the measurements of mean for each measurement
 	* There are 33 feature variables containing "mean()" from the 561 variables 
-5. Extract only the "mean" data from the merged "data" set into a new data set called "meandata"
-6. Rename the column names in "meandata" with the feature names containing "mean()"
+10. Extract only the "mean" data from the merged "data" set into a new data set called "meandata"
+11. Rename the column names in "meandata" with the feature names containing "mean()"
 	* The "meandata" has 10299 obs of 33 variables 
-7. Read "features.txt" to extract only the measurements of standard deviation for each measurement
+12. Read "features.txt" to extract only the measurements of standard deviation for each measurement
 	* There are 33 feature variables containing "std()" from the 561 variables 
-8. Extract only the "std" data from the merged "data" set into a new data set called "stddata"
-9. Rename the column names in "stddata" with the feature names containing std()"
+13. Extract only the "std" data from the merged "data" set into a new data set called "stddata"
+14. Rename the column names in "stddata" with the feature names containing std()"
 	* The "stddata" has 10299 obs of 33 variables 
-10. Use cbind() to combine the mean-only measurment data set "meandata" with the standard-deviation-only measurement data set "stddata" and create a new data set called "mergedmeastddata" 
+15. Use cbind() to combine the mean-only measurment data set "meandata" with the standard-deviation-only measurement data set "stddata" and create a new data set called "mergedmeastddata" 
 	* The "mergedmeastddata" has 10299 obs of 66 variables.
-11. Use sapply() to create a 2nd data set "averagedata" with the average of each variable for each activity and each subject
-	* The "averagedata"has 66 values for 66 variables
-	* The first 33 values are the measurements of mean related
-	* The second 33 valles are the measurements of standard deviation related
-12. When needed, use write.table() with row.names=FALSE to create a txt file "MergedMeanAndStdData.txt"
+16. Use cbind() to combine the "mergedmeastddata" with "subjectdata"
+	* The "mergedmeastddata" has 10299 obs of 67 variables. 
+	* tHE 67th column is the "Subject"
+17. Use cbind() to combine the "mergedmeastddata" with "activitydata"
+	* The "mergedmeastddata" has 10299 obs of 68 variables. 
+	* tHE 68th column is the "Activity" name
+18. The final data set has 10299 rows and 68 columns
+19. Use sapply() to create a 2nd data set "averagedata" with the average of each variable for each activity and each subject
+	* The "averagedata"has 180 rows
+	* The "averagedata"has 66 columns for 66 variables
+	* The first 33 column values are the measurements of mean related
+	* The second 33 column valles are the measurements of standard deviation related
+20. Use write.table() with row.names=FALSE to create a txt file "MergedMeanAndStdData.txt"
 
 
--- run_analysis.R
+++ run_analysis.R
@@ -2,57 +2,117 @@
 run_analysis <- function()
 {
         ## define the data file names need for this project
-        ## assume the data files are in the same working directory
-        testfile  <- "./X_test.txt"
-        trainfile <- "./X_train.txt"
+        ## assume the data files are in the same working directory and "test" and "train" sub-directories
         featuresfile <- "./features.txt"
+        activityfile <- "./activity_labels.txt"
+        
+        testfile  <- "./test/X_test.txt"
+        testactivity <- "./test/y_test.txt"
+        testsubject <- "./test/subject_test.txt"
+
+        trainfile <- "./train/X_train.txt"
+        trainactivity <- "./train/y_train.txt"
+        trainsubject <- "./train/subject_train.txt"
         
         #1. Merges the training and the test data sets to create one data set
         ##
-        ## Read test data - 2947 obs. of 561 variables
+        ## Read test data (2947 obs. of 561 variables), activity, and subject files
         testdata <- read.table(testfile)
+        testactivitydata <- read.table(testactivity)
+        testsubjectdata <- read.table(testsubject)
         
-        ## Read train data - 7352 obs. of 561 variables
+        ## Read train data (7352 obs. of 561 variables), activity, and subject files
         traindata <- read.table(trainfile)
+        trainactivitydata <- read.table(trainactivity)
+        trainsubjectdata <- read.table(trainsubject)
         
-        ## Merge train data with test data
+        ## Merge train data with test data to have one data set: 10299 rows & 561 columns
         data <- rbind(traindata, testdata)
         
-        #2. Extracts only the measurements on the mean and standard deviation for each measurement.
+        ## Merge the train activity with the test activity data - 10299 rows & 1 column
+        ## Rename the column as "Activity": 10299 rows & 1 column
+        activitydata <- rbind(trainactivitydata, testactivitydata)
+        names(activitydata) <- c("Activity")
+        
+        ## Merge the train subject with the test subject data - 10299 rows & 1 column
+        ## Rename the column as "Subject"
+        subjectdata <- rbind(trainsubjectdata, testsubjectdata)
+        names(subjectdata) <- c("Subject")
+        
         #3. Uses descriptive activity names to name the activities in the data set
-        #4. Appropriately labels the data set with descriptive variable names.
-        ## 
+        ##
+        ## Read the activity names
+        activitynames <- read.table(activityfile)
+        
+        ## Replace the activity number (1..6) with activity names in "activitydata" file
+        for (k in 1:nrow(activitynames)) 
+        {
+                activitydata[as.character(activitydata$Activity)==as.character(activitynames$V1[k]),] <- as.character(activitynames$V2[k])
+        }
+        
+        #2. Extracts only the measurements on the mean and standard deviation for each measurement.
+        ##
         ## Read the features data
         featuresdata <- read.table(featuresfile)
         
+        #4. Appropriately labels the data set with descriptive variable names.
+        ## 
         ## get the index of feature names containing "mean()" but exlcude meanFreq()
         indexofmean <- grep("mean", sub("meanFreq", "Freq", featuresdata$V2), fixed=TRUE)
         columnnamesofmean <- grep("mean", sub("meanFreq", "Freq", featuresdata$V2), fixed=TRUE, value=TRUE)
         
-        ## get the data for the columns containing "mean()"
+        ## get the data for the columns containing "mean()" from the merged oen data set 
+        ## rename column names with the original feature names - 10299 rows and 33 columns
         meandata <- data[indexofmean]
-        
-        ## rename column names with the original feature names
         names(meandata) <- columnnamesofmean
         
         ## get the index of feature names related to "std()" - 33 columns
         indexofstd <- grep("std", featuresdata$V2, fixed=TRUE)
         columnnamesofstd <- grep("std", featuresdata$V2, fixed=TRUE, value=TRUE)
         
-        ## get the data for the columns containing "std()"
+        ## get the data for the columns containing "std()" from the merged oen data set
+        ## rename column names with the original feature names -  - 10299 rows and 33 columns
         stddata <- data[indexofstd]
-        
-        ## rename column names with the original feature names
         names(stddata) <- columnnamesofstd
         
-        ## Merge the mean data and std data into one big data table: - should have 66 columns
-        mergedmeastddata <- cbind(meandata,stddata)
+        ## Merge the mean data and std data into one big data table: - 1029 rows & 66 columns
+        mergedmeanstddata <- cbind(meandata,stddata)
         
-        #5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-        averagedata <- sapply(mergedmeastddata, mean)
-        averagedata
+        ## Add the Subject column to the merged data - which is the 67th column
+        mergedmeanstddata <-cbind(mergedmeanstddata, subjectdata)
+        
+        ## Add the Activity column to the merged data - which is the 68th column
+        mergedmeanstddata <- cbind(mergedmeanstddata, activitydata)
+        
+        ## The mergedmeanstddata has 10299 rows and 68 columns after step 4
+        ## Column 1-33: measurements for mean()
+        ## Column 34- 66: measurements for std()
+        ## Column 67: Subject
+        ## Column 68: Activity
+        
+        
+        #5. From the data set in step 4, 
+        ##  Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+        ## 
+        ## The second data set will have 180 rows and 66 columns
+        for (k in 1:nrow(activitynames))
+        {
+                for (j in 1:30)
+                {
+                        ## extract the data set for Activity[k] & Subject[j]
+                        extracteddata <- mergedmeanstddata[as.character(mergedmeanstddata$Activity)==as.character(activitynames$V2[k]) & as.character(mergedmeanstddata$Subject)==as.character(j),]
+                        
+                        ## remove the last 2 columns to get the average data
+                        if (k==1 & j==1) {
+                                averagedata <- sapply(extracteddata[, 1:66], mean) 
+                        }
+                        else {
+                                averagedata <- rbind(averagedata, sapply(extracteddata[, 1:66], mean) )
+                        } 
+                }
+        }
         
         ## create a txt file with write.table() using row.names=FALSE 
-        ## write.table(averagedata, "./MergedMeanAndStdData.txt", row.names=FALSE)
+        write.table(averagedata, "./MergedMeanAndStdData.txt", row.names=FALSE)
         
 }
